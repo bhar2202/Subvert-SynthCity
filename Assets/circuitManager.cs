8b140079e325f26ae61079ceec27bc8187d6a762
Brack Harmon <brack02harmon@gmail.com>
2023-11-05 19:16:24 -0600

    Working subpuzzle

--- Assets/circuitManager.cs
+++ Assets/circuitManager.cs
@@ -4,10 +4,36 @@ using UnityEngine;
 
 public class circuitManager : MonoBehaviour
 {
+    private int numCells = 0;
+    
+    private int correctCells = 0;
+    private GameObject[] cells;
+    
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        cells = GameObject.FindGameObjectsWithTag("Cell");
+        numCells = cells.Length;
+        Debug.Log("num cells: " + numCells);
+    }
+
+    public void incrCorrCells()
+    {
+        correctCells++;
+        Debug.Log(correctCells);
+        if (correctCells == numCells)
+        {
+            foreach (GameObject item in cells)
+            {
+                item.GetComponent<SpriteRenderer>().sprite = item.GetComponent<CellController>().lightSprite;
+            }
+        }
+    }
+    
+    public void decrCorrCells()
+    {
+        correctCells--;
     }
 
     // Update is called once per frame
